d3917f2ef60920dfa6a7dccdc0782e627ee925f6
Valentin <V.Kovalenko.prog@gmail.com>
2022-10-24 14:46:49 +0300

    add project page

---
+++ src/helpers/ProjectList.jsx
@@ -0,0 +1,31 @@
+import Proj1 from "../assets/proj1.png";
+import Proj2 from "../assets/proj2.jpg";
+import Proj3 from "../assets/proj3.jpg";
+import Proj4 from "../assets/proj4.jpg";
+
+export const projectList = [
+    {
+        name: ' Visualizer',
+        image: Proj1
+    },
+    {
+        name: 'Dijkstra  Visualizer',
+        image: Proj2
+    },
+    {
+        name: 'Dijkstra Algorithm ',
+        image: Proj3
+    },
+    {
+        name: ' Algorithm Visualizer',
+        image: Proj4
+    },
+    {
+        name: 'Dijkstra ',
+        image: Proj3
+    },
+    {
+        name: ' Algorithm ',
+        image: Proj3
+    },
+]
--- src/pages/Projects/Projects.jsx
+++ src/pages/Projects/Projects.jsx
@@ -1,10 +1,19 @@
-import React from 'react'
+import React from "react";
+import ProjectItem from "../../components/ProjectItem/ProjectItem";
+import "./Projects.css";
+import { projectList } from "../../helpers/ProjectList";
 
 const Projects = () => {
   return (
-    <div>Projects</div>
-  )
-}
+    <div className="projects">
+      <h1>My Personal Projects</h1>
+      <div className="projectList">
+        {projectList.map((project) => {
+          return <ProjectItem name={project.name} image={project.image}/>
+        })}
+      </div>
+    </div>
+  );
+};
 
-
-export default Projects
+export default Projects;
